e708c9bb87bbe1d8bc67fb29b22c4ebebabf7df9
Just-A-Freshman <1504891917@qq.com>
2026-06-16 14:57:17 +0800

    删除冗余变量

---
+++ settings.py
@@ -0,0 +1,75 @@
+import json
+from pathlib import Path
+from typing import Literal
+from datetime import datetime
+import logging
+import ctypes
+
+
+class Setting(object):
+    config_path = Path("./config/setting.json")
+    temp_image_path = Path("./temp")
+    error_log = Path("./config/error.log")
+    accepted_exts = {'.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', '.webp'}
+    schedule_save_interval = 600000
+
+    def __init__(self) -> None:
+        Path.mkdir(Setting.temp_image_path, exist_ok=True)
+        self.__config: dict = self.load_settings()
+
+    def clean_log(self) -> None:
+        with open(Setting.error_log, "r", encoding="utf-8") as f:
+            content = f.readlines()
+        with open(Setting.error_log, "w", encoding="utf-8") as f:
+            for line in content:
+                try:
+                    target_date = datetime.fromisoformat(line.split(" ")[0])
+                    current_date = datetime.today()
+                    delta_days = (target_date - current_date).days
+                    if delta_days < 7:
+                        f.write(line)
+                except (ValueError, IndexError):
+                    pass
+
+    def get_config(self, config_type: Literal["model", "index", "function"], key: str):
+        config_type_key = f"{config_type}_config"
+        if key not in self.__config[config_type_key]:
+            self.__config[config_type_key][key] = None
+        return self.__config[config_type_key][key]
+
+    def modity_config(self, config_type: Literal["model", "index", "function"], key: str, content) -> None:
+        self.__config[f"{config_type}_config"][key] = content
+
+    def load_settings(self):
+        with open(Setting.config_path, encoding="utf-8") as f:
+            return json.load(f)
+
+    def save_settings(self) -> None:
+        with open(Setting.config_path, "w", encoding="utf-8") as f:
+            json.dump(self.__config, f, indent=4, ensure_ascii=False)
+
+
+
+class WinInfo(object):
+    version = "2.4.2"
+    repo_url = "https://github.com/Just-A-Freshman/VimgFind"
+    scale_factor = ctypes.windll.shcore.GetScaleFactorForDevice(0) / 100
+    ico_path = "config/favicon.ico"
+    title = "Vimgfind"
+    width = 830
+    height = 560
+
+    @staticmethod
+    def TkS(value: int | float, restore: bool = False) -> int:
+        if not restore:
+            return int(round(value * WinInfo.scale_factor, 0))
+        else:
+            return int(round(value / WinInfo.scale_factor, 0))
+
+
+logging.basicConfig(
+    filename=Setting.error_log,
+    level=logging.ERROR,
+    format='%(asctime)s -  %(message)s',
+    encoding='utf-8'
+)
