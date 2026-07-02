0c01bf8f3ac535352a0dae67ab295719a9124ec0
Just-A-Freshman <1504891917@qq.com>
2026-07-03 00:52:31 +0800

    chore:撤销上次提交

--- views/main_window.py
+++ views/main_window.py
@@ -42,7 +42,6 @@ class WinGUI(TkinterDnD.Tk):
         geometry = '%dx%d+%d+%d' % (width, height, (screenwidth - width) // 2, (screenheight - height) // 2)
         self.geometry(geometry)
         self.iconbitmap(WinInfo.ico_path)
-        self.option_add("*TCombobox*Listbox.font", ("微软雅黑", -30))
 
     def __set_notebook(self, parent) -> Notebook:
         notebook = Notebook(parent)
