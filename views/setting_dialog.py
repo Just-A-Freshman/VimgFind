363b8410c2af04218eddd8598cf5f12628ba1487
Just-A-Freshman <1504891917@qq.com>
2026-07-04 14:15:48 +0800

    feat:增加窗口置顶设置

--- views/setting_dialog.py
+++ views/setting_dialog.py
@@ -9,6 +9,7 @@ from settings import WinInfo, TkS
 class SettingDialog(tk.Toplevel):
     theme_combobox: Combobox
     maximize_checkbutton: Checkbutton
+    topmost_checkbutton: Checkbutton
     open_settings_file_btn: Button
     check_update_btn: Button
     
@@ -17,6 +18,7 @@ class SettingDialog(tk.Toplevel):
         self.__win(parent)
         self.theme_combobox = self.__set_theme_combobox()
         self.maximize_checkbutton = self.__set_maximize_checkbutton()
+        self.topmost_checkbutton = self.__set_topmost_checkbutton()
         self.open_settings_file_btn = self.__set_open_settings_file_btn()
         self.check_update_btn = self.__set_check_update_btn()
 
@@ -26,8 +28,8 @@ class SettingDialog(tk.Toplevel):
         self.iconbitmap(WinInfo.ico_path)
         self.transient(parent)
         self.update_idletasks()
-        win_w = TkS(200)
-        win_h = TkS(125)
+        win_w = TkS(220)
+        win_h = TkS(180)
         x = parent.winfo_rootx() + (parent.winfo_width() - win_w) // 2
         y = parent.winfo_rooty() + (parent.winfo_height() - win_h) // 2
         self.geometry(f"{win_w}x{win_h}+{x}+{y}")
@@ -38,7 +40,7 @@ class SettingDialog(tk.Toplevel):
         theme_setting_frame = Frame(self)
         theme_setting_frame.grid(row=1, column=1, columnspan=2, padx=(TkS(15), TkS(15)), pady=TkS(10), sticky=tk.W)
         Label(theme_setting_frame, text="界面主题：").pack(side=tk.LEFT)
-        combo = Combobox(theme_setting_frame, state="readonly", width=10, font=(WinInfo.default_font_family, WinInfo.default_font_size))
+        combo = Combobox(theme_setting_frame, state="readonly", width=TkS(6), font=(WinInfo.default_font_family, WinInfo.default_font_size))
         combo.pack(side=tk.LEFT)
         return combo
 
@@ -46,15 +48,20 @@ class SettingDialog(tk.Toplevel):
         check_btn = Checkbutton(self, text="  启动时最大化窗口")
         check_btn.grid(row=2, column=1, columnspan=2, pady=TkS(10), padx=(TkS(20), TkS(15)), sticky=tk.EW)
         return check_btn
+    
+    def __set_topmost_checkbutton(self) -> Checkbutton:
+        check_btn = Checkbutton(self, text="  将当前窗口置顶")
+        check_btn.grid(row=3, column=1, columnspan=2, pady=TkS(10), padx=(TkS(20), TkS(15)), sticky=tk.EW)
+        return check_btn
 
     def __set_check_update_btn(self) -> Button:
         check_update_btn = Button(self, text="检查更新", takefocus=True, style=LINK, cursor="hand2")
-        check_update_btn.grid(row=3, column=1, pady=TkS(10), padx=TkS(15), sticky=tk.W)
+        check_update_btn.grid(row=4, column=1, pady=TkS(10), padx=TkS(15), sticky=tk.W)
         return check_update_btn
 
     def __set_open_settings_file_btn(self) -> Button:
         open_settings_file_btn = Button(self, text="配置文件", takefocus=True, style=LINK, cursor="hand2")
-        open_settings_file_btn.grid(row=3, column=2, pady=TkS(10), padx=TkS(15))
+        open_settings_file_btn.grid(row=4, column=2, pady=TkS(10), padx=TkS(15))
         return open_settings_file_btn
 
     def destroy(self) -> None:
