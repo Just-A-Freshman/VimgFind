1e8bf68031a844b2c6873ad44896eadaf5adf446
Just-A-Freshman <1504891917@qq.com>
2026-07-09 20:29:04 +0800

    feat:改用线程池管理图片加载

--- views/widgets/image_loader.py
+++ views/widgets/image_loader.py
@@ -1,5 +1,5 @@
-from threading import Thread
 from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
 import queue
 
 from PIL import ImageTk, ImageOps
@@ -11,48 +11,37 @@ LoaderResult = namedtuple("LoaderResult", ["item", "size", "photo", "error"])
 
 
 class ImageLoader:
-    __slots__ = ("task_queue", "result_queue", "threads", "running")
+    __slots__ = ("_executor", "_result_queue", "_running")
 
     def __init__(self) -> None:
-        self.task_queue: queue.Queue[tuple] = queue.Queue()
-        self.result_queue: queue.Queue[LoaderResult] = queue.Queue()
-        self.threads: list[Thread] = []
-        self.running = True
-        for _ in range(10):
-            thread = Thread(target=self._worker, daemon=True)
-            thread.start()
-            self.threads.append(thread)
+        self._result_queue: queue.Queue[LoaderResult] = queue.Queue()
+        self._executor = ThreadPoolExecutor(max_workers=10, thread_name_prefix="img_loader")
+        self._running = True
 
     def add_task(self, item: str, image_path: str, thumbnail_size: int) -> None:
-        self.task_queue.put((item, image_path, thumbnail_size))
+        self._executor.submit(self._process_one, item, image_path, thumbnail_size)
 
-    def _worker(self) -> None:
-        while self.running:
-            try:
-                item, image_path, thumbnail_size = self.task_queue.get(timeout=1)
-            except queue.Empty:
-                continue
-            img = image_ops.parse_image_from_path(image_path)
-            if img is None:
-                self.result_queue.put(LoaderResult(
-                    item=item, size=(0, 0), photo=None, error="加载图片失败！"
-                ))
-            else:
-                img = ImageOps.exif_transpose(img) or img
-                width, height = img.size
-                img.thumbnail((thumbnail_size, thumbnail_size))
-                self.result_queue.put(LoaderResult(
-                    item=item,
-                    size=(width, height),
-                    photo=img,
-                    error=""
-                ))
-            self.task_queue.task_done()
+    def _process_one(self, item: str, image_path: str, thumbnail_size: int) -> None:
+        img = image_ops.parse_image_from_path(image_path)
+        if img is None:
+            self._result_queue.put(LoaderResult(
+                item=item, size=(0, 0), photo=None, error="加载图片失败！"
+            ))
+        else:
+            img = ImageOps.exif_transpose(img) or img
+            width, height = img.size
+            img.thumbnail((thumbnail_size, thumbnail_size))
+            self._result_queue.put(LoaderResult(
+                item=item,
+                size=(width, height),
+                photo=img,
+                error=""
+            ))
 
     def get_results(self) -> list[LoaderResult]:
         results = []
-        while not self.result_queue.empty():
-            result = self.result_queue.get_nowait()
+        while not self._result_queue.empty():
+            result = self._result_queue.get_nowait()
             photo = ImageTk.PhotoImage(result.photo) if result.photo is not None else None
             results.append(LoaderResult(
                 item=result.item, size=result.size, photo=photo, error=result.error
@@ -60,6 +49,5 @@ class ImageLoader:
         return results
 
     def stop(self) -> None:
-        self.running = False
-        for thread in self.threads:
-            thread.join(timeout=1)
+        self._running = False
+        self._executor.shutdown(wait=False, cancel_futures=True)
