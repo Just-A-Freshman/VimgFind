76d4a95a7d3a48a4d5a9f87c90178466345377b4
Just-A-Freshman <1504891917@qq.com>
2026-07-21 15:17:51 +0800

    feat:为is_path_under添加归一化接口

--- utils/file_ops.py
+++ utils/file_ops.py
@@ -192,9 +192,10 @@ def normalize_path(path: str) -> str:
     return os.path.normcase(os.path.realpath(path))
 
 
-def is_path_under(path: str, parent_dir: str) -> bool:
-    path = normalize_path(path)
-    parent_dir = normalize_path(parent_dir)
+def is_path_under(path: str, parent_dir: str, *, normalized: bool = False) -> bool:
+    if not normalized:
+        path = normalize_path(path)
+        parent_dir = normalize_path(parent_dir)
     parent_with_sep = parent_dir.rstrip(os.sep) + os.sep
     return path.startswith(parent_with_sep)
 
