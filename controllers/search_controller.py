bd8239ac3ca42d0733bce5bd5b2937e2976850bd
Just-A-Freshman <1504891917@qq.com>
2026-08-05 12:09:00 +0800

    fix:搜索时给定返回结果数为负数导致出错

--- controllers/search_controller.py
+++ controllers/search_controller.py
@@ -160,7 +160,7 @@ class SearchController:
     def set_preview_result_count(self, max_match_count: int) -> None:
         assert self.app.search_tools
         try:
-            self.app.setting.app.max_match_count = min(max_match_count, 500)
+            self.app.setting.app.max_match_count = max(min(max_match_count, 500), 1)
             self.app.search_tools.update_max_match_count(self.app.setting.app.max_match_count)
             self.resend_last_search()
         except TypeError:
